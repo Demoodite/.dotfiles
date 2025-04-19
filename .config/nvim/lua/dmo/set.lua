a25993775ac175f867e42b548d82972714a1f38e
Demoodite <m.jafari1379@gmail.com>
2025-04-19 23:00:38 +0330

    Lua formatting

--- .config/nvim/lua/dmo/set.lua
+++ .config/nvim/lua/dmo/set.lua
@@ -1,11 +1,24 @@
-vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.undofile = true
-vim.opt.showmode = false
-vim.opt.signcolumn = "yes"
-vim.opt.inccommand = "split"
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
+local g = vim.g
+-- local o = vim.o
+local opt = vim.opt
+
+g.python_recommended_style = 0
+g.rust_recommended_style = 0
+g.have_nerd_font = true
+
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = 4 -- Number of spaces for autoindent
+opt.softtabstop = 4 -- Number of spaces per tab press
+opt.tabstop = 4 -- Number of spaces a tab counts for
+
+opt.smartindent = true
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.undofile = true
+opt.showmode = false
+opt.signcolumn = "yes"
+opt.inccommand = "split"
+opt.cursorline = true
+opt.scrolloff = 10
 -- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,17 +1,17 @@
 return {
-    {
-	"lewis6991/gitsigns.nvim",
-	config = function ()
-	    require("gitsigns").setup()
-	end,
-	opts = {
-	    signs = {
-		add = { text = "+" },
-		change = { text = "~" },
-		delete = { text = "_" },
-		topdelete = { text = "‾" },
-		changedelete = { text = "~" },
-	    },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
 	},
-    },
 }
