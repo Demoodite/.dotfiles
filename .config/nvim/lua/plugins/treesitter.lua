18d12e37a597cf9a1c119fec820080e57335b870
Demoodite <m.jafari1379@gmail.com>
2025-04-22 22:43:28 +0330

    Lua formatting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,41 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		main = "nvim-treesitter.configs",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-			config = function()
-				require("nvim-treesitter.configs").setup({
-					incremental_selection = {
-						enable = true,
-						keymaps = {
-							init_selection = "gnn",
-							node_incremental = "grn",
-							scope_incremental = "grc",
-							node_decremental = "grm",
-						},
-					},
-				})
-			end,
-		},
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        main = "nvim-treesitter.configs",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "diff",
+                "html",
+                "lua",
+                "luadoc",
+                "markdown",
+                "markdown_inline",
+                "query",
+                "vim",
+                "vimdoc",
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "ruby" },
+            },
+            indent = { enable = true, disable = { "ruby" } },
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    incremental_selection = {
+                        enable = true,
+                        keymaps = {
+                            init_selection = "gnn",
+                            node_incremental = "grn",
+                            scope_incremental = "grc",
+                            node_decremental = "grm",
+                        },
+                    },
+                })
+            end,
+        },
+    },
 }
