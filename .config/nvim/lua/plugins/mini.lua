d089b9076bb840d93a75749300e4b2125ec67a8e
Demoodite <m.jafari1379@gmail.com>
2025-05-15 03:27:56 +0330

    Removed redundant mini.completion

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
         "echasnovski/mini.nvim",
         config = function()
             require("mini.icons").setup()
-            require("mini.completion").setup()
             require("mini.ai").setup({ n_lines = 500 })
             require("mini.surround").setup()
             local statusline = require("mini.statusline")
