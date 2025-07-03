b14989e57064989dcdec5ed6b74570b6232be1c1
Demoodite <m.jafari1379@gmail.com>
2025-07-03 17:10:09 +0330

    nvim BASEDpyright

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -136,7 +136,7 @@ return {
                 neocmake = {},
                 clangd = {},
                 -- gopls = {},
-                pyright = {},
+                basedpyright = {},
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
