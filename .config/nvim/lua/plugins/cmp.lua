b40d04ba01d217d150f49372e88aeb926570693b
Mahdi Jafari <m.jafari1379@gmail.com>
2026-06-16 15:07:55 +0330

    *cmp.lua fixed

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,47 +1,21 @@
 return {
     {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    {
         "saghen/blink.cmp",
         event = "VimEnter",
         version = "1.*",
-        dependencies = {
-            {
-                "L3MON4D3/LuaSnip",
-                version = "2.*",
-                build = (function()
-                    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
-                opts = {},
-            },
-            "folke/lazydev.nvim",
-        },
         --- @module 'blink.cmp'
         --- @type blink.cmp.Config
         opts = {
             keymap = {
-                -- 'default' (recommended) for mappings similar to built-in completions
-                --   <c-y> to accept ([y]es) the completion.
-                --    This will auto-import if your LSP supports it.
-                --    This will expand snippets if the LSP sent a snippet.
-                -- 'super-tab' for tab to accept
-                -- 'enter' for enter to accept
-                -- 'none' for no mappings
-                --
-                -- For an understanding of why the 'default' preset is recommended,
-                -- you will need to read `:help ins-completion`
-                --
-                -- No, but seriously. Please read `:help ins-completion`, it is really good!
-                --
-                -- All presets have the following mappings:
-                -- <tab>/<s-tab>: move to right/left of your snippet expansion
-                -- <c-space>: Open menu or open docs if already open
-                -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-                -- <c-e>: Hide menu
-                -- <c-k>: Toggle signature help
-                --
-                -- See :h blink-cmp-config-keymap for defining your own keymap
                 preset = "enter",
             },
 
@@ -54,13 +28,16 @@ return {
             },
 
             sources = {
-                default = { "lsp", "path", "snippets", "lazydev" },
+                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
                 providers = {
-                    lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        score_offset = 100,
+                    },
                 },
             },
 
-            snippets = { preset = "luasnip" },
             fuzzy = { implementation = "lua" },
             signature = { enabled = true },
         },
